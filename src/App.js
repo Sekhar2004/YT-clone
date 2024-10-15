52992daf6d91c6fe2e06c8b444396e27914d993b
Sekhar2004 <jeerurajasekharreddy@gmail.com>
2024-10-15 10:36:33 +0530

    added remaining files

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
+console.log(Box);
+
+const App = () => (
+  <Router>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </Router>
+);
+
+export default App;
